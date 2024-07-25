01862cf215a75b125159ebbbd5272864ac74443b
Srikasun <sri07072005@gmail.com>
2024-07-25 21:52:43 +0530

    added json

--- blogging/lib/models/user.dart
+++ blogging/lib/models/user.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class User {
   final String id;
   final String name;
@@ -11,4 +14,30 @@ class User {
       required this.email,
       required this.token,
       required this.password});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'email': email,
+      'token': token,
+      'password': password,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      map['_id'] as String,
+      map['name'] as String,
+      map['email'] as String,
+      map['token'] as String,
+      map['password'] as String,
+      id: '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
 }
