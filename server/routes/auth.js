41d12b9ee7efab3e69224802746a7a882cef32c0
rahulranesh <rahulranesh590@gmail.com>
2024-07-25 20:46:35 +0530

    fourth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express=require ("express");
 const User = require("../models/user");
 const authRouter=express.Router();
 //Sign-Up 
-authRouter.post("/api/signup",async(req,res)=>
+authRouter.post("/api/registerUser",async(req,res)=>
 {
     try{
         const {name,email,password}=req.body;
@@ -34,6 +34,35 @@ authRouter.post("/api/signup",async(req,res)=>
 {
 res.status(500).json({error:e.message});
 }
+});
+
+//Sign-in
+
+authRouter.post("/api/login",async (req,res)=>{
+
+    try{
+        const {email,password}=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            return res
+            .status(400)
+            .json({msg:" User with this email does not exist"});
+        }
+
+        const isMatch=await bcryptjs.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({msg:"Incorrect Password"});
+
+        }
+        const token=jwt.sign({id:user._id},"passwordKey");
+        res.json({
+            token,...user._doc
+        });
+
+    }catch(e){
+        res.status(500).json({error:e.message});
+        }
+
 });
 
 module.exports=authRouter;
